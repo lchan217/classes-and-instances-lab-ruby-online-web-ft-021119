361d1e38f38630e4f8ed1b88f81f3143f0cf774e
Lizzie Chan <lchan217@gmail.com>
2019-03-07 01:56:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,3 @@
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-
-html = open('https://www.goodreads.com/choiceawards/best-of-the-best-2018')
-doc = Nokogiri::HTML(html)
-
-array = doc.css('.inlineblock.pollAnswer.resultShown').css('strong.uitext.result').text.split("\n")
-
-separated_array = array.each_slice(5).to_a
-
-binding.pry
 
 
 
@@ -19,11 +7,9 @@ binding.pry
 
 
 
+class Dog 
+end 
 
-
-# class Dog 
-# end 
-
-# fido = Dog.new 
-#   lassie = Dog.new
-#   snoopy = Dog.new
+fido = Dog.new 
+  lassie = Dog.new
+  snoopy = Dog.new
