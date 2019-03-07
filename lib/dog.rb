52112a6ebb587afd6fa49787a92857a3367cd62c
Lizzie Chan <lchan217@gmail.com>
2019-03-07 01:56:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,15 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
+html = open('https://www.goodreads.com/choiceawards/best-of-the-best-2018')
+doc = Nokogiri::HTML(html)
 
+array = doc.css('.inlineblock.pollAnswer.resultShown').css('strong.uitext.result').text.split("\n")
 
+separated_array = array.each_slice(5).to_a
+
+binding.pry
 
 
 
